2cbf53a28f1bb2e3decf635e2d33a81e851efa48
alejandro.perez.acosta@hotmail.com <aperezNWO>
2024-06-01 00:48:31 -0500

    dark theme

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // or 'media' or false
+  content: [
+    "./src/**/*.{html,ts}", // Adjust this line according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
